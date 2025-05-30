e0843df50e9c56023b50696f2c024e60d5e599b1
KristianOksavik <49029873+KristianOksavik@users.noreply.github.com>
2025-05-30 14:24:14 +0200

    Delete Simulation directory

--- Simulation/Bash-Scripts/propagation.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for i in $(seq 1 200);#$(seq 1 200);
-do
-    num=$(($i))
-    python ePIE-Reconstruction.py $num
-done
-#python moviemaker.py "/cluster/home/kristaok/reconstruction/tempimages/"
